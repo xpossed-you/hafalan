ec691291bc3eae080763835eff6922fa61b29453
xpossed-you <coolgamerindo@gmail.com>
2024-09-22 03:30:40 +0800

    nice

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'bg-image': "url('https://t4.ftcdn.net/jpg/01/73/34/83/360_F_173348335_6yTtlk3lxN3tlesLybLG667TIPxpT2wQ.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
+
